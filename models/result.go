b4426bd64d3c691d270c410fae151eb59aef9aa0
Amrita Bindukalpa <amrita.bindukalpa@gmail.com>
2022-04-24 17:01:06 +0100

    added routine to get results

--- models/result.go
+++ models/result.go
@@ -1,7 +1,22 @@
 package models
 
+import (
+	"sbg-api/determinator"
+	"sbg-api/generators"
+)
+
 type Result struct {
 	Number int
 	Colour string
 	IsOdd  bool
+}
+
+func NewResult() Result {
+	n := generators.RandomNumber()
+	r := Result{
+		Number: n,
+		Colour: determinator.Colour(n),
+		IsOdd:  determinator.IsOdd(n),
+	}
+	return r
 }
