f0fd6101fbac8c633b4cbff930f3395738b5c8c7
Amrita Bindukalpa <amrita.bindukalpa@gmail.com>
2022-04-24 19:53:08 +0100

    create method to calculate wins and add tests

---
+++ betresults/wins.go
@@ -0,0 +1,23 @@
+package betresults
+
+import (
+	"sbg-api/models"
+)
+
+const NumberWin = "Your number won!"
+const ColourWin = "Your colour won!"
+const ParityWin = "Your parity won!"
+
+func GetWins(b models.Bets, r models.Result) models.Winning {
+	var w models.Winning
+	if b.Number == r.Number {
+		w.NumberWin = NumberWin
+	}
+	if b.Colour == r.Colour {
+		w.ColourWin = ColourWin
+	}
+	if b.IsOdd == r.IsOdd {
+		w.ParityWin = ParityWin
+	}
+	return w
+}
---
+++ betresults/wins_test.go
@@ -0,0 +1,46 @@
+package betresults_test
+
+import (
+	"sbg-api/betresults"
+	"sbg-api/models"
+	"testing"
+)
+
+type WinsTestCase struct {
+	bet      models.Bets
+	result   models.Result
+	expected models.Winning
+}
+
+var winsTests = []WinsTestCase{
+	{
+		bet:      models.Bets{2, "red", true},
+		result:   models.Result{2, "red", true},
+		expected: models.Winning{betresults.NumberWin, betresults.ColourWin, betresults.ParityWin},
+	},
+	{
+		bet:      models.Bets{2, "red", true},
+		result:   models.Result{3, "red", true},
+		expected: models.Winning{"", betresults.ColourWin, betresults.ParityWin},
+	},
+	{
+		bet:      models.Bets{2, "red", true},
+		result:   models.Result{3, "black", false},
+		expected: models.Winning{"", "", ""},
+	},
+}
+
+func TestColourOutput(t *testing.T) {
+	for _, tc := range winsTests {
+		actual := betresults.GetWins(tc.bet, tc.result)
+		if actual.NumberWin != tc.expected.NumberWin {
+			t.Fatalf("Number win did not match")
+		}
+		if actual.ColourWin != tc.expected.ColourWin {
+			t.Fatalf("Colour win did not match")
+		}
+		if actual.ParityWin != tc.expected.ParityWin {
+			t.Fatalf("Parity win did not match")
+		}
+	}
+}
